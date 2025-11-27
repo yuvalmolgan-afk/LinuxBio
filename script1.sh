efd48da8548690d67b6240c169148e1410c9e499
yuvalmolgan-afk <yuvalmolgan@gmail.com>
2025-11-27 08:48:12 +0000

    12

--- script1.sh
+++ script1.sh
@@ -13,3 +13,10 @@ sum2=$((sum+3))
 echo "sum = $sum"
 echo "sum2 = $sum2"
 echo "pwd = `pwd`"
+
+mkdir -p test
+touch test/a.txt
+touch test/b.txt
+for ((i=1; i<=5; i++)); do
+touch test/$i.txt
+done
