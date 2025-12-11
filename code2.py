f14c35580f0ca0082e0b7f63892e11ec48e320cf
yuvalmolgan-afk <yuvalmolgan@gmail.com>
2025-12-11 07:46:15 +0000

    ds

--- code2.py
+++ code2.py
@@ -2,12 +2,12 @@ import sys
 
 # total arguments
 n = len(sys.argv)
-print("Total arguments passed:", n)
+#print("Total arguments passed:", n)
 
 # Arguments passed
-print("\nName of Python script:", sys.argv[0])
+#print("\nName of Python script:", sys.argv[0])
 
-print("\nArguments passed:", end = " ")
+#print("\nArguments passed:", end = " ")
 for i in range(1, n):
 	print(sys.argv[i], end = " ")
 	
@@ -17,4 +17,4 @@ Sum = 0
 for i in range(1, n):
 	Sum += int(sys.argv[i])
 	
-print("\n\nResult:", Sum)
+print("---->", Sum)
